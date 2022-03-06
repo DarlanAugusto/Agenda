a6cbad973b0752db77929a451dfde54b0c602df4
Luiz <luis231103@gmail.com>
2022-03-06 14:17:14 -0300

    Iniciando projeto

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+include_once("config/url.php");
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Agenda</title>
+    <!-- FONT AWESOME -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- CSS -->
+    <link rel="stylesheet" href="<?=$BASE_URL?>css/styles.css">
+</head>
+<body>
+    <h1>Teste</h1>
+    <i class="fas fa-ad"></i>
+</body>
+</html>
