30cb892719e79eefdf81dcf36ffa5a0f77a09992
Luiz <luis231103@gmail.com>
2022-03-09 00:36:12 -0300

    Criando html e css da home

--- config/process.php
+++ config/process.php
@@ -2,6 +2,8 @@
 
 session_start();
 
+$contacts = [];
+
 include_once("conexao.php");
 include_once("url.php");
 
@@ -9,7 +11,7 @@ $query = "SELECT * FROM contacts";
 
 $stmt = $con->prepare($query);
 
-$stmt->execute;
+$stmt->execute();
 
 $contacts = $stmt->fetchAll();
 
--- index.php
+++ index.php
@@ -4,8 +4,40 @@ include_once("templates/header.php");
 
 ?>
     
-    <h1>Teste</h1>
-    <i class="fas fa-ad"></i>
+<div class="container">
+    <?php if(isset($printMsg) && $printMsg != ''): ?>
+        <p id="msg"><?php echo $printMsg?></p>
+    <?php endif;?>
+    <h1 id="main-title">Minha Agenda</h1>
+    <?php if(count($contacts ) > 0 ): ?>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Nome</th>
+                    <th scope="col">Telefone</th>
+                    <th scope="col"></th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach($contacts as $contact): ?>
+                    <tr>
+                        <td scope="row"><?php echo $contact["id"]; ?></td>
+                        <td scope="row"><?php echo $contact["nome"]; ?></td>
+                        <td scope="row"><?php echo $contact["phone"]; ?></td>
+                        <td class = "actions">
+                            <a href="<?php echo $BASE_URL ?>#"><i class="fa-solid fa-eye"></i></a>
+                            <a href="<?php echo $BASE_URL ?>#"><i class="fa-solid fa-pen-to-square"></i></a>
+                            <a href="<?php echo $BASE_URL ?>#"><i class="fa-solid fa-trash-can"></i></a>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    <?php else: ?>
+        <p id="empty-text"> Ainda não há contatos na sua agenda, <a href="<?php echo $BASE_URL?>create.php">Clique aqui para adicionar!</a> </p>    
+    <?php endif; ?>
+</div>
 
 
 <?php 
