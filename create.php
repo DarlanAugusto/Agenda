c68949b5e6b9014494219260d88827fc9ce42317
Luiz <luis231103@gmail.com>
2022-03-10 17:32:37 -0300

    Fianlizando css da pagina de criar contato

--- create.php
+++ create.php
@@ -4,8 +4,24 @@ include_once("templates/header.php");
 
 ?>
     
-    <h1>Teste</h1>
-    <i class="fas fa-ad"></i>
+    <h1 id="main-title">Criar Contato</h1>
+    <div id="container-create">
+        <form action="<?php echo $BASE_URL?>config/process.php" method ="POST">
+            <input type="hidden" name="type" value ="create">
+
+            <div class="create">
+                <label class="subtitle" for="name">Nome:</label>
+                <input class="upp" type="text" name="name" id="name" required>
+
+                <label class="subtitle" for="phone">Telefone:</label>
+                <input class="upp" type="text" name="phone" id="phone" required>
+
+                <label class="subtitle" for="obs">Observações:</label>
+                <textarea name="observations" id="observations" cols="30" rows="4"></textarea>
+            </div>
+            <button class="btn" type="submit">Cadastrar</button>
+        </form>
+    </div>
 
 
 <?php 
